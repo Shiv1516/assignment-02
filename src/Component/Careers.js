80ae6a0c5efd6d3d11953133dfcf165221ad8995
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-12-07 23:23:00 +0530

    file update

--- src/Component/Careers.js
+++ src/Component/Careers.js
@@ -2,14 +2,78 @@ import React from "react";
 
 function Careers() {
   return (
-    <div className="career-section wrapper df fww ptb48">
-      <div className="career-cards flx25">
-        <div className="career-card mlr16 tac">
-          <div className="career-"></div>
-
-          <h3 className="career-advance fs20 mb8 fw6">1000+</h3>
-          <h4 className="career-advance fs20 mb8 fw6">Careers Advanced</h4>
+    <div className="container df fww jcsb wrapper ptb48 mtb16">
+      <div className="gauge-container mlr16 df fdc aic">
+        <div className="gauge mb16">
+          <svg viewBox="0 0 100 50">
+            <path d="M 10 50 A 40 40 0 0 1 90 50" className="background" />
+            <path
+              d="M 10 50 A 40 40 0 0 1 90 50"
+              className="progress stoke25"
+            />
+          </svg>
+          <div className="value-text pt32">80%</div>
         </div>
+        <h3 classNameName="career-advance fs20 fw6 mb12 fw6">1000+</h3>
+        <h4 classNameName="career-advance fs20 fw6 mb12 fw6">
+          Careers Advanced
+        </h4>
+      </div>
+      <div className="gauge-container mlr16 df fdc aic">
+        <div className="gauge mb16">
+          <svg viewBox="0 0 100 50">
+            <path d="M 10 50 A 40 40 0 0 1 90 50" className="background" />
+            <path
+              d="M 10 50 A 40 40 0 0 1 90 50"
+              className="progress stoke75"
+            />
+          </svg>
+          <div className="value-text pt32">40%</div>
+        </div>
+        <h3 classNameName="career-advance fs20 mb12 fw6">1000+</h3>
+        <h4 classNameName="career-advance fs20 mb12 fw6">Careers Advanced</h4>
+      </div>
+      <div className="gauge-container mlr16 df fdc aic">
+        <div className="gauge mb16">
+          <svg viewBox="0 0 100 50">
+            <path d="M 10 50 A 40 40 0 0 1 90 50" className="background" />
+            <path
+              d="M 10 50 A 40 40 0 0 1 90 50"
+              className="progress stoke100"
+            />
+          </svg>
+          <div className="value-text pt32">20%</div>
+        </div>
+        <h3 classNameName="career-advance fs20 mb12 fw6">1000+</h3>
+        <h4 classNameName="career-advance fs20 mb12 fw6">Careers Advanced</h4>
+      </div>
+      <div className="gauge-container mlr16 df fdc aic">
+        <div className="gauge mb16">
+          <svg viewBox="0 0 100 50">
+            <path d="M 10 50 A 40 40 0 0 1 90 50" className="background" />
+            <path
+              d="M 10 50 A 40 40 0 0 1 90 50"
+              className="progress stoke63"
+            />
+          </svg>
+          <div className="value-text pt32">50%</div>
+        </div>
+        <h3 classNameName="career-advance fs20 mb12 fw6">1000+</h3>
+        <h4 classNameName="career-advance fs20 mb12 fw6">Careers Advanced</h4>
+      </div>
+      <div className="gauge-container mlr16 df fdc aic">
+        <div className="gauge mb16">
+          <svg viewBox="0 0 100 50">
+            <path d="M 10 50 A 40 40 0 0 1 90 50" className="background" />
+            <path
+              d="M 10 50 A 40 40 0 0 1 90 50"
+              className="progress stoke38"
+            />
+          </svg>
+          <div className="value-text pt32">70%</div>
+        </div>
+        <h3 classNameName="career-advance fs20 mb12 fw6">1000+</h3>
+        <h4 classNameName="career-advance fs20 mb12 fw6">Careers Advanced</h4>
       </div>
     </div>
   );
